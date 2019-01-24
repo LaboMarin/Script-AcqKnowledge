ccdfc310eec7a6a9b79a462fe1e9d0f4e49dd3f6
Clément Bourguignon <35606879+clementbourguignon@users.noreply.github.com>
2019-01-24 18:03:57 -0500

    First commit

---
+++ config.m
@@ -0,0 +1,4 @@
+folder = 'D:\Clement\Code\Lab-Marin\data';
+namepattern = 'Parti?cipant[ _]?(?<ID>\d{2})(_| - )(?<session>[a-zA-Z]+).+';
+groups = 'D:\Clement\Code\Lab-Marin\Assignation.csv';
+paradigms = 'D:\Clement\Code\Lab-Marin\Conditions.csv';
